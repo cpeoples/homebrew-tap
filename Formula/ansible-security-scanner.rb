4e03253800d835a979c989658bdae0bdab12b545
Chris Peoples <699389+cpeoples@users.noreply.github.com>
2026-06-01 19:00:08 -0400

    ansible-security-scanner 0.1.18 (#1)

---
+++ Formula/ansible-security-scanner.rb
@@ -0,0 +1,86 @@
+class AnsibleSecurityScanner < Formula
+  include Language::Python::Virtualenv
+
+  desc "Static security scanner (SAST) for Ansible playbooks, roles, and collections"
+  homepage "https://github.com/cpeoples/ansible-security-scanner"
+  url "https://files.pythonhosted.org/packages/fa/59/2f60d5276ef482394b66dc1dc159961230c9388e4f2548a0a8a4ee0291de/ansible_security_scanner-0.1.18.tar.gz"
+  sha256 "7ae460f833648eb0660e757033f3eea6787607d1b85ead4d57d9f5c1862ebfb5"
+  license "Apache-2.0"
+
+  depends_on "libyaml"
+  depends_on "python@3.13"
+
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/19/14/2c5dd9f512b66549ae92767a9c7b330ae88e1932ca57876909410251fe13/anyio-4.13.0.tar.gz"
+    sha256 "334b70e641fd2221c1505b3890c69882fe4a2df910cba14d97019b90b24439dc"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/f3/ce/ee2ecad540810a79593028e88299baeae54d346cc7a0d94b6199988b89b1/certifi-2026.5.20.tar.gz"
+    sha256 "69dea482ab64caa7b9f6aba1c6bf48bb6a5448d1c0f1b17ab42ad8c763a5344d"
+  end
+
+  resource "h11" do
+    url "https://files.pythonhosted.org/packages/01/ee/02a2c011bdab74c6fb3c75474d40b3052059d95df7e73351460c8588d963/h11-0.16.0.tar.gz"
+    sha256 "4e35b956cf45792e4caa5885e69fba00bdbc6ffafbfa020300e549b208ee5ff1"
+  end
+
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/06/94/82699a10bca87a5556c9c59b5963f2d039dbd239f25bc2a63907a05a14cb/httpcore-1.0.9.tar.gz"
+    sha256 "6e34463af53fd2ab5d807f399a9b45ea31c3dfa2276f15a2c3f00afff6e176e8"
+  end
+
+  resource "httpx" do
+    url "https://files.pythonhosted.org/packages/b1/df/48c586a5fe32a0f01324ee087459e112ebb7224f646c0b5023f5e79e9956/httpx-0.28.1.tar.gz"
+    sha256 "75e98c5f16b0f35b567856f597f06ff2270a374470a5c2392242528e3e3e42fc"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/b9/28/99c51f664567218d824af024c0251650fb27e4ca066df188dab0769c5b91/idna-3.17.tar.gz"
+    sha256 "5eb0cb53bc467c12eadcf6de83163ad8527cec9416f44b9b61b19caedad2b87f"
+  end
+
+  resource "jinja2" do
+    url "https://files.pythonhosted.org/packages/df/bf/f7da0350254c0ed7c72f3e33cef02e048281fec7ecec5f032d4aac52226b/jinja2-3.1.6.tar.gz"
+    sha256 "0137fb05990d35f1275a587e9aee6d56da821fc83491a0fb838183be43f66d6d"
+  end
+
+  resource "markupsafe" do
+    url "https://files.pythonhosted.org/packages/7e/99/7690b6d4034fffd95959cbe0c02de8deb3098cc577c67bb6a24fe5d7caa7/markupsafe-3.0.3.tar.gz"
+    sha256 "722695808f4b6457b320fdc131280796bdceb04ab50fe1795cd540799ebe1698"
+  end
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
+  end
+
+  resource "sniffio" do
+    url "https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd/sniffio-1.3.1.tar.gz"
+    sha256 "f4324edc670a0f49750a81b895f35c3adb843cca46f0530f79fc1babb23789dc"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "--directory", shell_output("#{bin}/ansible-security-scanner --help")
+
+    (testpath/"playbook.yml").write <<~YAML
+      - hosts: all
+        tasks:
+          - name: pipe a script into sh
+            shell: curl https://get.example.com/install.sh | sh
+    YAML
+
+    require "json"
+    report = JSON.parse(shell_output("#{bin}/ansible-security-scanner --directory #{testpath} --format json", 2))
+    refute_empty report.fetch("findings"), "scanner should flag a curl|sh pipeline"
+  end
+end
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # homebrew-tap
 
-Homebrew tap for tools by [@cpeoples](https://github.com/cpeoples). Setup in progress.
+Homebrew tap for tools by [@cpeoples](https://github.com/cpeoples).
+
+## Install
+
+```sh
+brew install cpeoples/tap/ansible-security-scanner
+```
+
+[`ansible-security-scanner`](Formula/ansible-security-scanner.rb) is a static
+security scanner for Ansible playbooks, roles, and collections. See the
+[upstream repository](https://github.com/cpeoples/ansible-security-scanner)
+for usage and rule documentation.
+
+## License
+
+Formula files are MIT licensed. Each packaged tool retains its own license.
