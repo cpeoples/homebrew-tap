bbc1d130e3d17f5c5fabd7da7bf07b222eb4c479
Chris Peoples <699389+cpeoples@users.noreply.github.com>
2026-07-21 15:44:47 -0400

    Add grackle formula (#27)

---
+++ Formula/grackle.rb
@@ -0,0 +1,54 @@
+class Grackle < Formula
+  desc "Detects fork-triggerable CI coding agents that can write to the repository"
+  homepage "https://github.com/cpeoples/grackle"
+  version "0.1.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/cpeoples/grackle/releases/download/v0.1.0/grackle-v0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/cpeoples/grackle/releases/download/v0.1.0/grackle-v0.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/cpeoples/grackle/releases/download/v0.1.0/grackle-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/cpeoples/grackle/releases/download/v0.1.0/grackle-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "grackle"
+  end
+
+  test do
+    assert_match "grackle", shell_output("#{bin}/grackle --version")
+    assert_match "rules.", shell_output("#{bin}/grackle --list-rules")
+
+    (testpath/"wf.yml").write <<~YAML
+      on:
+        issue_comment:
+          types: [created]
+      jobs:
+        agent:
+          permissions:
+            contents: write
+          steps:
+            - run: pip install aider-chat
+            - run: aider --yes --message "$(cat task.md)"
+    YAML
+
+    require "json"
+    report = JSON.parse(shell_output("#{bin}/grackle --format json #{testpath}/wf.yml", 1))
+    refute_empty report, "grackle should flag a fork-triggerable write-capable agent"
+  end
+end
